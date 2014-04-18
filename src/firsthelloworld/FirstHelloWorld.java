15ad6c4c51662a4a95453b256db24516174a11c1
g12933nm <g12933nm@s30404.tsuda.ac.jp>
2014-04-18 11:18:38 +0900

    FirstHelloWorldです

---
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firsthelloworld;
+
+/**
+ *
+ * @author g12933nm
+ */
+public class FirstHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
